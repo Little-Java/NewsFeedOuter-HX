59ff494a780500c869eb1c3129a098f29d9b3017
liuqi <33508405+Little-Java@users.noreply.github.com>
2020-12-28 19:18:31 +0800

    更改用户实例名称

--- src/main/java/com/example/hongxing/mapper/PAgentMapper.java
+++ src/main/java/com/example/hongxing/mapper/PAgentMapper.java
@@ -6,11 +6,15 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @Mapper
 public interface PAgentMapper extends BaseMapper<PAgentEntity> {
 
-    PAgentEntity selectUserByName(@Param("usAccounts") String usAccounts);
+    PAgentEntity selectUserByName(@Param("agentId") String agentId);
 
     PAgentEntity selectUserById(@Param("usId") Integer usId);
+
+    List<PAgentEntity> queryUserList();
 }
